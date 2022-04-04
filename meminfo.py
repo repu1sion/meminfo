6e55eeba9ce1b50c473df420a355906c3428e1b2
repu1sion <bearrailgun@gmail.com>
2022-04-04 16:51:33 +0300

    meminfo update

--- meminfo.py
+++ meminfo.py
@@ -4,15 +4,17 @@ import sys
 
 def print_ram():
 	# dict() constructor builds a dictionary in meminfo from key-value pairs.
-	meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
-	mem_kib = meminfo['MemTotal']  # e.g. 3921852
-	mem_free = meminfo['MemFree']
-	mem_cached = meminfo['Cached']
-	mem_avail = meminfo['MemAvailable']
+    meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
+    mem_kib = meminfo['MemTotal']  # e.g. 3921852
+    mem_free = meminfo['MemFree']
+    mem_cached = meminfo['Cached']
+    mem_buffers = meminfo['Buffers']
+    mem_avail = meminfo['MemAvailable']
+    mem_used = mem_kib - mem_free - mem_buffers - mem_cached
 
-	print("RAM | Total: %d Mb |" % (mem_kib/1024), "Free: %d Mb |" %  (mem_free/1024),
-		 "Cached: %d Mb |" %(mem_cached/1024),"Available: %d Mb" %(mem_avail/1024) )
-
+    print("RAM | Total: %d Mb |" % (mem_kib/1024), "Free: %d Mb |" %  (mem_free/1024),
+    "Cached: %d Mb |" %(mem_cached/1024), "Used: %d Mb |" %(mem_used/1024),
+    "Available: %d Mb" %(mem_avail/1024) )
 
 
 '''
